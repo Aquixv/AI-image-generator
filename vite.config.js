cf55237b3e66133d602acc805dc04eec2a56f109
unknown <dareakintundealamu@gmail.com>
2025-11-12 09:52:22 +0100

    slight modifications

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/hf-image': { 
-        // TARGET REMAINS THE NEW ROUTER BASE:
-        target: 'https://router.huggingface.co', 
-        changeOrigin: true, 
-        
-        // ⚠️ NEW REWRITE: 
-        // It converts: /hf-image/stabilityai/stable-diffusion-2-1
-        // TO:           /hf-inference/models/stabilityai/stable-diffusion-2-1
-        rewrite: (path) => path.replace(/^\/hf-image/, '/hf-inference/models'), 
-      },
-    },
   },
 });
